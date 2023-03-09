9aa7380ef9a4675e316ec7b2281328489ee0e4b4
android16-rj <65335642+android16-rj@users.noreply.github.com>
2023-03-09 14:42:35 +0000

    formatted design and added api route. api is yet to be configured.

--- sypto_app.py
+++ sypto_app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+"""
+BOTS = [
+  {
+  id: 1,
+  title: "Athena Top 5"},
+  {
+  id: 1,
+  title: "Athena Top 10"},
+  {
+  id: 1,
+  title: "Athena Custom"}
+]
+# flask templating syntax for dynamic data 
+"""
 @app.route("/")
 def Hello_world():
   return render_template('home.html')
+  #return render_template('home.html', bots = BOTS)
+
+@app.route("/api/current_price")
+def trade_history(coin):
+  pass
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
